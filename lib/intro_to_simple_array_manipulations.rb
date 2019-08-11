87ceb6382aece09ed3f49c86632896db1aea1eca
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-11 19:22:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -54,6 +54,6 @@ end
 using_delete(["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"], "Steven")
 
 def using_delete_at(array, integer)
-  array.delete(array, 2)
+  array.delete(array, integer)
 end
 using_delete_at(["Johnny 5", "R2D2", "Robocop"], 2)
