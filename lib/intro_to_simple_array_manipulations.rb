62fcdb1e265f322003a82a081dd78aea53572440
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-11 19:06:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -32,3 +32,7 @@ def using_concat(my_favorite_things, more_favs)
   my_favorite_things.concat(more_favs)
 end
 using_concat(["raindrops on roses", "whiskers on kittens"], ["sports cars", "flatiron school"])
+
+def using_uniq(array)
+  array.uniq
+end
